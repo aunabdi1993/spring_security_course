f6a8e07e86c3eb54e0354e5b919bf1a6801ef089
Aun <aun@oneminutebookreview.com>
2022-10-11 22:32:41 +0100

    added all appUser and registration from other project

---
+++ src/main/java/com/example/demo/appUser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.kalaq.demo.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.kalaq.demo.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
